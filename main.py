4c5efd5053edd99decb1435c22131aecfdc6ee51
J-Bills <billsjamal@gmail.com>
2024-05-16 22:00:45 -0700

    accidental addition of a bunch of unnessecary stuff but who cares

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import requests
+import os
+from twilio.rest import Client
+
 
 with open('./openweather_acc.txt') as info_file:
     contents = info_file.readlines()
     contents = [line.strip() for line in contents]
+    
+with open('./twilio_acc.txt') as twilio_info:
+    secrets = twilio_info.readlines()
+    secrets = [line.strip() for line in secrets]
 
 
 q: str
 appid: str
 
-q = input("input city name").lower()
+# q = input("input city name").lower()
 appid = contents[2]
 
 
 
-paramemters = {
-    'q' : q,
-    'appid' : appid
-}
+# paramemters = {
+#     'q' : q,
+#     'appid' : appid
+# }
 
 
 LAT = 37.739651
@@ -25,7 +32,8 @@ LONG = 121.425224
 lat_parameters = {
     'lat': LAT,
     "lon":LONG,
-    'appid' : appid
+    'appid' : appid,
+    'cnt': 4
 }
 
 
@@ -33,8 +41,18 @@ lat_parameters = {
 # response = requests.get(f"https://api.openweathermap.org/data/2.5/weather", params=paramemters)
 # response.raise_for_status()
 
-response = requests.get(f"https://api.openweathermap.org/data/2.5/forcast", params=lat_paramemters)
+response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast", params=lat_parameters)
 print(response.status_code)
 data = response.json()
 
-print(data)
+
+weather_id_list = list()
+for num in range(0,3):
+    weather_id = data['list'][num]['weather'][0]['id']
+    weather_id_list.append(weather_id)
+    
+if any(weather_id < 700 for weather_id in weather_id_list):
+    print('bring an umbrella')
+else:
+    print("lovely day")
+
